807a16ef43e1944dba3994ac8084140cf932b183
Shusmita Dey <129603661+shusmitaOSD@users.noreply.github.com>
2023-04-01 20:32:00 +0600

    Add files via upload

---
+++ includes/leftbar1.php
@@ -0,0 +1,61 @@
+
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="Habib" class="img-circle profile-img">
+                                <h6 class="title">Admin</h6>
+                                <small class="info">Administrator</small>
+                            </div>
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Main Category</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard1.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Appearance</span>
+                                    </li>
+
+<li class="has-children">
+                    <a href="#"><i class="fa fa-file-text"></i> <span>Subjects</span> <i class="fa fa-angle-right arrow"></i></a>
+                        <ul class="child-nav">
+                            <li><a href="create-subject1.php"><i class="fa fa-bars"></i> <span>Create Subject</span></a></li>
+                                            <li><a href="manage-subjects1.php"><i class="fa fa fa-server"></i> <span>Manage Subjects</span></a></li>
+                                           <li><a href="add-subjectcombination1.php"><i class="fa fa-newspaper-o"></i> <span>Add Subject Combination </span></a></li>
+                                           <li><a href="manage-subjectcombination1.php"><i class="fa fa-newspaper-o"></i> <span>Manage Subject Combination </span></a></li>
+                                        </ul>
+                                    </li>
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Students</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students1.php"><i class="fa fa-bars"></i> <span>Add Students</span></a></li>
+                                            <li><a href="manage-students1.php"><i class="fa fa fa-server"></i> <span>Manage Students</span></a></li>
+                                           
+                                        </ul>
+
+                                    </li>
+   
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-info-circle"></i> <span>Result</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-result.php"><i class="fa fa-bars"></i> <span>Add Result</span></a></li>
+                                            <li><a href="manage-results.php"><i class="fa fa fa-server"></i> <span>Manage Result</span></a></li>
+                                           
+                                        </ul>        </li>
+
+
+
+
+                                        <li><a href="change-password.php"><i class="fa fa fa-server"></i> <span> Admin Change Password</span></a></li>
+                                           
+                            
+                            </div>
+
+                        </div>
+
+                    </div>
